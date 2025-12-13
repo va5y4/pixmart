5504771ee9e29f9d393001433384f1f375094465
Vasilis Moysidis <vasilis.moysidis@proton.me>
2025-12-13 09:52:49 +0200

    update project name and enhance header navigation with dynamic data

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import vituum from 'vituum';
 import nunjucks from '@vituum/vite-plugin-nunjucks';
 import twig from '@vituum/vite-plugin-twig';
 import postcss from '@vituum/vite-plugin-postcss';
-
 import imageOptimizer from '@bro-academy/vite-plugin-image-optimizer';
 import svgSprite from '@bro-academy/vite-plugin-svg-sprite';
+import common from './src/data/common.json' assert { type: 'json' };
 
 export default defineConfig({
   build: {
@@ -14,7 +14,9 @@ export default defineConfig({
   plugins: [
     svgSprite(),
     vituum(),
-    twig(),
+    twig({
+      globals: { common }
+    }),
     nunjucks(),
     postcss(),
     imageOptimizer(),
