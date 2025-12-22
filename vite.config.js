71ef56db8bf251fad6f14cafb0b678f1d97c615b
Vasilis Moysidis <vasilis.moysidis@proton.me>
2025-12-22 22:35:52 +0200

    initialize vite

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import vituum from 'vituum';
 import nunjucks from '@vituum/vite-plugin-nunjucks';
 import twig from '@vituum/vite-plugin-twig';
 import postcss from '@vituum/vite-plugin-postcss';
+
 import imageOptimizer from '@bro-academy/vite-plugin-image-optimizer';
 import svgSprite from '@bro-academy/vite-plugin-svg-sprite';
-import common from './src/data/common.json' with { type: 'json' };
 
 export default defineConfig({
   build: {
@@ -14,9 +14,7 @@ export default defineConfig({
   plugins: [
     svgSprite(),
     vituum(),
-    twig({
-      globals: { common }
-    }),
+    twig(),
     nunjucks(),
     postcss(),
     imageOptimizer(),
