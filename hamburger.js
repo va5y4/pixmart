b7c5227facea0786b45cd16fd07b60186e9d9aa6
Vasilis Moysidis <vasilis.moysidis@proton.me>
2026-06-11 18:37:28 +0300

    upd

--- hamburger.js
+++ hamburger.js
@@ -4,4 +4,6 @@ const navLinks = document.getElementById('nav-links');
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navLinks.classList.toggle('active');
+
+  document.documentElement.classList.toggle('no-scroll');
 });
